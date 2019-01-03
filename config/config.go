0831133354fb306392dceb9f14b249f25f436e7d
Tobias B <germansuperioz@yahoo.de>
2019-01-03 12:53:33 +0100

    Added config

--- config/config.go
+++ config/config.go
@@ -2,7 +2,22 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"runtime"
+	"strings"
+)
+
+const (
+	dirUnix    = "/etc/artemis"
+	dirWindows = "%s/.artemis"
+
+	configFile = "config.json"
+
+	windowsOS      = "windows"
+	homeDriveEnv   = "HOMEDRIVE"
+	homePathEnv    = "HOMEPATH"
+	userProfileEnv = "USERPROFILE"
 )
 
 type ClusterConfig struct {
@@ -17,16 +32,48 @@ type ClusterConfig struct {
 	ClusterSize       int `json:"clusterSize"`
 }
 
-func Load(file string) (ClusterConfig, error) {
-	var config ClusterConfig
+func GetRootDirectory() string {
+	if runtime.GOOS == windowsOS {
+		home := os.Getenv(homeDriveEnv) + os.Getenv(homePathEnv)
+		if home == "" {
+			home = os.Getenv(userProfileEnv)
+		}
+		return fmt.Sprintf(dirWindows, strings.Replace(home, "\\", "/", -1))
+	}
+	return dirUnix
+}
+
+func Load(def ClusterConfig) (ClusterConfig, error) {
+	dir := GetRootDirectory() + "/" + configFile
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.Mkdir(GetRootDirectory(), os.ModePerm)
+		if err != nil {
+			return def, err
+		}
+
+		_, err = os.Create(dir)
+		if err != nil {
+			return def, err
+		}
+	}
+
+	c, err := readInConfig(dir, def)
+	if err != nil {
+		return def, err
+	}
+	return c, nil
+}
+
+// reads config struct from given file directory
+func readInConfig(file string, config ClusterConfig) (ClusterConfig, error) {
 	configFile, err := os.Open(file)
 	if err != nil {
-		return ClusterConfig{}, err
+		return config, err
 	}
 	jsonParser := json.NewDecoder(configFile)
 	err = jsonParser.Decode(&config)
 	if err != nil {
-		return ClusterConfig{}, err
+		return config, err
 	}
 
 	err = configFile.Close()
---
+++ config/config_test.go
@@ -0,0 +1,10 @@
+package config
+
+/*func TestLoad(t *testing.T) {
+	c, err := Load(ClusterConfig{})
+	if err != nil {
+		t.Skip(err)
+	}
+
+	fmt.Println(c)
+}*/
