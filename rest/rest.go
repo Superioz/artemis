e3b6df769f07593ecac43349caae62ffeebd70bc
Tobias B <germansuperioz@yahoo.de>
2019-01-04 14:52:23 +0100

    Added basic rest setup

--- rest/rest.go
+++ rest/rest.go
@@ -1,11 +1,28 @@
 package rest
 
 import (
-	"github.com/buaazp/fasthttprouter"
+	"fmt"
 	"github.com/valyala/fasthttp"
 )
 
-func lock() {
-	fasthttp.AcquireArgs()
-	fasthttprouter.New()
+type Server struct {
+	host string
+	port int
+}
+
+func New(host string, port int) *Server {
+	return &Server{
+		host: host,
+		port: port,
+	}
+}
+
+func (s *Server) Up() error {
+	router := GetRouter()
+
+	err := fasthttp.ListenAndServe(fmt.Sprintf("%s:%d", s.host, s.port), router.Handler)
+	if err != nil {
+		return err
+	}
+	return nil
 }
